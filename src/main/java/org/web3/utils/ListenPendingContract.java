a58d9bdfbe451e43ed01553ad905a1729f217530
EthanOk <sdzdj01@163.com>
2023-06-30 17:29:06 +0800

    modified:   src/main/java/org/web3/utils/ListenPendingContract.java

--- src/main/java/org/web3/utils/ListenPendingContract.java
+++ src/main/java/org/web3/utils/ListenPendingContract.java
@@ -14,19 +14,26 @@ public class ListenPendingContract {
 
         Web3j web3j = Web3j
                 .build(new HttpService(RPC));
-        String contract = "0x71eE06999F6D5f66AcA3c12e45656362fD9D031f";
+        String opensea = "0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC";
+        String yungou = "0xb0E3773e3E02d0A1653F90345Bc8889fC820E230";
         Disposable subscription = web3j.pendingTransactionFlowable()
                 .subscribe(tx -> {
                     String to = tx.getTo();
-                    System.out.println(tx.getHash());
-                    System.out.println(tx.getFrom());
-                    System.out.println(tx.getTo());
-
-                    if (to != null && tx.getTo().toLowerCase() == contract.toLowerCase()) {
-                        System.out.println("1111111111111111111111111");
-                        System.out.println("New pending transaction: " + tx.getHash());
+                    // System.out.println(tx.getHash());
+                    // System.out.println(tx.getFrom());
+                    // System.out.println(tx.getTo());
+                    // System.out.println("````````````````````");
+                    // System.out.println(tx.getInput());
+                    if (to != null) {
+                        if (to.equalsIgnoreCase(opensea)) {
+                            System.out.println("Opensea pending transaction: " + tx.getHash());
+                            System.out.println(to);
+                        } else if (to.equalsIgnoreCase(yungou)) {
+                            System.out.println("YunGou pending transaction: " + tx.getHash());
+                            System.out.println(to);
+                        }
+                    } else {
                         System.out.println(to);
-                        System.out.println(contract);
                     }
 
                 });
