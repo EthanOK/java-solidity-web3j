847cd7ceb93042d3b57cf7a7d0078366744159bf
EthanOk <sdzdj01@163.com>
2023-08-29 17:58:19 +0800

    new file:   src/main/java/org/web3/utils/PostTransferEventsBSC.java 	new file:   src/main/java/org/web3/utils/PostTransferEventsETH.java 	new file:   src/main/java/org/websocket/BSCWebSocketClient.java

---
+++ src/main/java/org/web3/utils/PostTransferEventsBSC.java
@@ -0,0 +1,206 @@
+package org.web3.utils;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.mysql.ConncetDB;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.FunctionReturnDecoder;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Hash;
+import org.web3j.utils.EnsUtils;
+import org.web3j.utils.Numeric;
+import io.github.cdimascio.dotenv.Dotenv;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class PostTransferEventsBSC {
+    static Dotenv dotenv = Dotenv.load();
+    // ANKR_HTTP_BSC QUIKNODE_HTTP_BSC
+    static String QUIKNODE_HTTP_BSC = dotenv.get("ANKR_HTTP_BSC");
+    static Connection connection = ConncetDB.getConnect();
+
+    public static void main(String[] args) {
+        // postTransferEventsInRange("18017459", "18017461");
+
+        // TODO: 每3秒请求一次，获得最新区块数据
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        Runnable printTask = () -> {
+            postTransferEvents();
+        };
+        // 初始延迟0秒，然后每隔10秒执行一次任务
+        executorService.scheduleAtFixedRate(printTask, 0, 3, TimeUnit.SECONDS);
+    }
+
+    public static void postTransferEventsInRange(String fromBlock, String toBlock) {
+        OkHttpClient client = new OkHttpClient();
+        String fromBlockHex = Numeric.toHexStringWithPrefix(new BigInteger(fromBlock));
+        String toBlockHex = Numeric.toHexStringWithPrefix(new BigInteger(toBlock));
+        MediaType mediaType = MediaType.parse("application/json");
+        String requestBody_ = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getLogs\",\"params\":[{\"fromBlock\":\"%s\",\"toBlock\":\"%s\",\"topics\":[\"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef\"]}],\"id\":1}";
+        String requestBody = String.format(requestBody_, fromBlockHex, toBlockHex);
+        Request request = new Request.Builder()
+                .url(QUIKNODE_HTTP_BSC)
+                .post(RequestBody.create(mediaType, requestBody))
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+
+                JSONObject json = new JSONObject(responseBody);
+
+                JSONArray results = json.getJSONArray("result");
+                // System.out.println(results.toString());
+                if (results.length() > 0) {
+                    // handle Response Result
+                    handleResponseResult(results);
+                }
+            } else {
+                System.out.println("POST request failed with response code: " + response.code());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void postTransferEvents() {
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        String requestBody = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getLogs\",\"params\":[{\"topics\":[\"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef\"]}],\"id\":1}";
+        Request request = new Request.Builder()
+                .url(QUIKNODE_HTTP_BSC)
+                .post(RequestBody.create(mediaType, requestBody))
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+
+                JSONObject json = new JSONObject(responseBody);
+
+                JSONArray results = json.getJSONArray("result");
+                // System.out.println(results.toString());
+                if (results.length() > 0) {
+                    // handle Response Result
+                    handleResponseResult(results);
+                }
+            } else {
+                System.out.println("POST request failed with response code: " + response.code());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void handleResponseResult(JSONArray results) {
+        /*
+         * {
+         * "address": "0xf6afc05fccea5a53f22a3e39ffee861e016bd9a0",
+         * "blockHash":
+         * "0xcb2a112831cc2a7ab0d6e93b0dda0aba046de1391c294da381f8e34e049198fc",
+         * "blockNumber": "0x11297f1",
+         * "data": "0x00000000000000000000000000000000000000000003ae67cbce6ebf24144000",
+         * "logIndex": "0x2",
+         * "removed": false,
+         * "topics": [
+         * "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+         * "0x000000000000000000000000120051a72966950b8ce12eb5496b5d1eeec1541b",
+         * "0x000000000000000000000000e977791b2cd891e5dcd54d3a530ae413e8d2f242"
+         * ],
+         * "transactionHash":
+         * "0x24106b7d2950fa908d441381ab10fe8848305e81b901ac2e40f83771e1f64a62",
+         * "transactionIndex": "0x2"
+         * }
+         */
+        for (int i = 0; i < results.length(); i++) {
+            JSONObject result = results.getJSONObject(i);
+            String transactionHash = result.getString("transactionHash");
+            String blockNumber = result.getString("blockNumber");
+            String token = result.getString("address");
+            JSONArray topics = result.getJSONArray("topics");
+
+            if (topics.length() == 4) {
+                String from = topics.getString(1);
+                String to = topics.getString(2);
+                String fromAddress = FunctionReturnDecoder.decodeAddress(from);
+                String toAddress = FunctionReturnDecoder.decodeAddress(to);
+                if (!fromAddress.equals(EnsUtils.EMPTY_ADDRESS)
+                        && !toAddress.equals(EnsUtils.EMPTY_ADDRESS)) {
+
+                    String tokenId = topics.getString(3);
+
+                    BigInteger blockNumberBig = Numeric.toBigInt(blockNumber);
+
+                    BigInteger tokenIdBig = Numeric.toBigInt(tokenId);
+
+                    // TODO: check data of on sale
+                    try {
+                        String encodeData = FunctionEncoder
+                                .encodeConstructor(Arrays.<Type>asList(new Address(token), new Address(fromAddress),
+                                        new Address(toAddress),
+                                        new Uint256(tokenIdBig),
+                                        new Uint256(blockNumberBig)));
+                        String encodeDataHash = Hash.sha3("0x" + encodeData);
+
+                        String insertQuery = "INSERT IGNORE INTO aggregator_ethan.event_transfer_erc721_bsc "
+                                + "(token, tokenId, fromAddress, toAddress, blockNumber, transactionHash, encodeDataHash) "
+                                + "VALUES(?,?,?,?,?,?,?)";
+                        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery,
+                                PreparedStatement.RETURN_GENERATED_KEYS);
+
+                        // Set values for parameters
+                        preparedStatement.setString(1, token);
+                        preparedStatement.setString(2, tokenIdBig.toString());
+                        preparedStatement.setString(3, fromAddress);
+                        preparedStatement.setString(4, toAddress);
+                        preparedStatement.setInt(5, blockNumberBig.intValue());
+                        preparedStatement.setString(6, transactionHash);
+                        preparedStatement.setString(7, encodeDataHash);
+
+                        int rowsAffected = preparedStatement.executeUpdate();
+                        if (rowsAffected > 0) {
+                            // 获取生成的主键值
+                            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                            if (generatedKeys.next()) {
+                                long generatedId = generatedKeys.getLong(1);
+                                System.out.println("event_transfer_erc721 insert Id: " + generatedId);
+                            }
+
+                            generatedKeys.close();
+                            preparedStatement.close();
+                        }
+
+                    } catch (SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ src/main/java/org/websocket/BSCWebSocketClient.java
@@ -0,0 +1,204 @@
+package org.websocket;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Map;
+import java.math.BigInteger;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.mysql.ConncetDB;
+import org.web3.utils.PostTransferEventsInRange;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.FunctionReturnDecoder;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Hash;
+import org.web3j.utils.EnsUtils;
+import org.web3j.utils.Numeric;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class BSCWebSocketClient extends WebSocketClient {
+    static Dotenv dotenv = Dotenv.load();
+
+    // TODO：没有免费的wss服务节点，使用量太大
+    static String QUIKNODE_WSS_BSC = dotenv.get("QUIKNODE_WSS_BSC");
+    static Connection connection = ConncetDB.getConnect();
+    static final int RECONNECT_INTERVAL = 5000; // 初始重连间隔
+    static String subscribeRequest = "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_subscribe\",\"params\":[\"logs\", {\"topics\":[\"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef\"]}]}";
+    private static WebSocketClient client;
+    static BigInteger lastBlockNumber;
+
+    public BSCWebSocketClient(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+    }
+
+    public BSCWebSocketClient(URI serverURI) {
+        super(serverURI);
+    }
+
+    public BSCWebSocketClient(URI serverUri, Map<String, String> httpHeaders) {
+        super(serverUri, httpHeaders);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+
+        System.out.println("opened connection");
+        send(subscribeRequest);
+
+    }
+
+    @Override
+    public void onMessage(String message) {
+        // System.out.println(message);
+        // handleMessage
+        handleMessage(message);
+
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        // The close codes are documented in class org.java_websocket.framing.CloseFrame
+        System.out.println(
+                "Connection closed by " + (remote ? "remote peer" : "us") + " Code: " + code + " Reason: "
+                        + reason);
+
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        ex.printStackTrace();
+        // if the error is fatal then onClose will be called additionally
+    }
+
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
+        try {
+            client = new BSCWebSocketClient(new URI(QUIKNODE_WSS_BSC));
+            client.connect();
+            Thread.sleep(RECONNECT_INTERVAL);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        // while (true) {
+        // if (client.isClosed()) {
+        // // 如果已经关闭，推送记录的最后一次 blockNumber 进队列
+        // // push lastBlockNumber
+        // // System.out.println(lastBlockNumber);
+        // // 处理队列 （lastBlockNumber, lastBlockNumber + 1）
+        // System.out.println("Reconnecting to INFURA...");
+        // reconnectToINFURA();
+        // }
+        // try {
+        // Thread.sleep(RECONNECT_INTERVAL);
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // }
+        // }
+
+    }
+
+    private static void reconnectToINFURA() throws URISyntaxException, InterruptedException {
+        // System.out.println("isClosed: " + client.isClosed());
+        client.close();
+        client = new BSCWebSocketClient(new URI(QUIKNODE_WSS_BSC));
+        client.connect();
+        Thread.sleep(RECONNECT_INTERVAL);
+        // 处理中断的区块数据
+        PostTransferEventsInRange.postTransferEventsInRange(lastBlockNumber.toString(),
+                lastBlockNumber.add(new BigInteger("2")).toString());
+
+    }
+
+    private void handleMessage(String message) {
+        // String 转 Json
+        JSONObject json = new JSONObject(message);
+
+        if (json.has("method")) {
+            JSONObject params = json.getJSONObject("params");
+            JSONObject result = params.getJSONObject("result");
+            String transactionHash = result.getString("transactionHash");
+            String blockNumber = result.getString("blockNumber");
+            String token = result.getString("address");
+
+            JSONArray topics = result.getJSONArray("topics");
+            // ERC20: topics.length() == 3
+            // ERC721: topics.length() == 4
+
+            if (topics.length() == 4) {
+                String from = topics.getString(1);
+                String to = topics.getString(2);
+                String fromAddress = FunctionReturnDecoder.decodeAddress(from);
+                String toAddress = FunctionReturnDecoder.decodeAddress(to);
+                if (!fromAddress.equals(EnsUtils.EMPTY_ADDRESS)
+                        && !toAddress.equals(EnsUtils.EMPTY_ADDRESS)) {
+
+                    String tokenId = topics.getString(3);
+
+                    BigInteger blockNumberBig = Numeric.toBigInt(blockNumber);
+
+                    BigInteger tokenIdBig = Numeric.toBigInt(tokenId);
+
+                    lastBlockNumber = blockNumberBig;
+
+                    // TODO: check data of on sale
+                    try {
+
+                        String encodeData = FunctionEncoder
+                                .encodeConstructor(Arrays.<Type>asList(new Address(token), new Address(fromAddress),
+                                        new Address(toAddress),
+                                        new Uint256(tokenIdBig),
+                                        new Uint256(blockNumberBig)));
+                        String encodeDataHash = Hash.sha3("0x" + encodeData);
+
+                        String insertQuery = "INSERT IGNORE INTO aggregator_ethan.event_transfer_erc721 "
+                                + "(token, tokenId, fromAddress, toAddress, blockNumber, transactionHash, encodeDataHash) "
+                                + "VALUES(?,?,?,?,?,?,?)";
+                        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery,
+                                PreparedStatement.RETURN_GENERATED_KEYS);
+
+                        // Set values for parameters
+                        preparedStatement.setString(1, token);
+                        preparedStatement.setString(2, tokenIdBig.toString());
+                        preparedStatement.setString(3, fromAddress);
+                        preparedStatement.setString(4, toAddress);
+                        preparedStatement.setInt(5, blockNumberBig.intValue());
+                        preparedStatement.setString(6, transactionHash);
+                        preparedStatement.setString(7, encodeDataHash);
+
+                        int rowsAffected = preparedStatement.executeUpdate();
+                        if (rowsAffected > 0) {
+                            // 获取生成的主键值
+                            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                            if (generatedKeys.next()) {
+                                long generatedId = generatedKeys.getLong(1);
+                                System.out.println("event_transfer_erc721 insert Id: " + generatedId);
+                            }
+
+                            generatedKeys.close();
+                            preparedStatement.close();
+                        }
+
+                    } catch (SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
